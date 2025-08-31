b5483b6deae78eda5a5f8cc424bd5645e20e3ce0
monkynhko <42342888+monkynhko@users.noreply.github.com>
2025-08-31 20:15:50 +0200

    Delete movies directory

--- movies/src/main/java/com/example/movies/repository/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.movies.repository;
-
-import com.example.movies.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> { }
